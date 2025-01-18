0f69f904cdf80bb46c9e24a90836f279fd137ecf
Bennet Steem <bennet.steem@gmail.com>
2025-01-18 14:09:52 -0700

    fullscreen+menu

--- dashboard.py
+++ dashboard.py
@@ -6,14 +6,22 @@ from tkinter import *
 def run_app():
     # Create the main window
     window = Tk()
+    window.state("zoomed")              # set the window to fullscreen
 
+    # Add the menu
     menu = Menu(window)
     window.config(menu=menu)
-    dash_menu = Menu(menu)
-    menu.add_cascade(label="Dashboard", menu=dash_menu)
+    file_menu = Menu(menu)
+    menu.add_cascade(label="File", menu=file_menu)
+    view_menu = Menu(menu)
+    menu.add_cascade(label="View", menu=view_menu)
+    
+    # TODO: add command to redirect to dashboard
+    file_menu.add_command(label="Import CSV")
+    file_menu.add_command(label="Close App", command=window.destroy)
 
-    # TODO: add command
-    dash_menu.add_command(label="Open")
+    # TODO: add command to redirect to dashboard
+    view_menu.add_command(label="Dashboard")
     
 
     dash_button = tk.Button(window, text="Dashboard", command=window.destroy)

04cd84b1b358a9279ad79c0a7d9454cb84807836
Bennet Steem <bennet.steem@gmail.com>
2025-01-21 19:02:41 -0700

    added-body
    Added instruction elements to the body of the homepage

--- Homepage.py
+++ Homepage.py
@@ -2,10 +2,25 @@
 
 import streamlit as st
 
-st.set_page_config(page_title="AutoDash", page_icon="😎")
+# Config
+st.set_page_config(page_title="AutoDashly", page_icon="😎")
 
-st.title("AutoDash")
+# Side bar
 st.sidebar.success("Select a page")
+
+# \/ Body 
+st.title("Welcome to AutoDashly!!! 👋")
+
+st.text("An intuitive automation dashboard designed to show users visualizations of their net worth")
+
+st.subheader("Getting Started")
+st.text("To get started you have two options.")
+
+st.subheader("Option 1")
+st.text("To see how the dashboard works, got to Dashboard")
+
+st.subheader("Option 2")
+st.text("Clone my github repo and run the server on your local machine.")
 
 # Saving items to session state
 if "my_input" not in st.session_state:

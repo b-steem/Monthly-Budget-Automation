d8a1e98a3085982269fe77668c06749a748db535
Bennet Steem <bennet.steem@gmail.com>
2025-01-26 22:02:08 -0700

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -8,6 +8,8 @@ st.title("Dashboard")
 # TODO: Pickup from here - https://blog.streamlit.io/crafting-a-dashboard-app-in-python-using-streamlit/
 try:
     df = pd.read_csv('data/transactions.csv')
+    print(df.head(20))
+    st.line_chart(df, x="Date", y="Amount")
 
 except:
     st.text("Please upload some data to get started!!!")
@@ -16,4 +18,4 @@ except:
 
 
 # accessing session state in other pages
-st.write("You have entered", st.session_state["my_input"])
+# st.write("You have entered", st.session_state["my_input"])

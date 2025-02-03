1200fca3735689bed0b69d2a1480e9b2ff33cdc9
Bennet Steem <bennet.steem@gmail.com>
2025-02-03 13:28:59 -0700

    updated csv upload

--- pages/Upload.py
+++ pages/Upload.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Upload")
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # Read the file as csv
-    pass
+    df = pd.read_csv(uploaded_file)
+    st.dataframe(df, width=1800, height=1200)
     

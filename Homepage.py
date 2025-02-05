8d6b00b97cfeb9d62f3b8e0eafdd76e2d342c0a2
Bennet Steem <bennet.steem@gmail.com>
2025-02-05 16:48:59 -0700

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -27,12 +27,13 @@ st.button("Upload CSV")
 st.subheader("Option 2")
 st.text("Clone my github repo and run the server on your local machine.")
 
-# Saving items to session state
-if "my_input" not in st.session_state:
-    st.session_state["my_input"] = ""
 
-my_input = st.text_input("Input a text here", st.session_state["my_input"])
-submit = st.button("Submit")
-if submit:
-    st.session_state["my_input"] = my_input
-    st.write("You have entered: ", my_input)
+# Saving items to session state
+# if "my_input" not in st.session_state:
+#     st.session_state["my_input"] = ""
+
+# my_input = st.text_input("Input a text here", st.session_state["my_input"])
+# submit = st.button("Submit")
+# if submit:
+#     st.session_state["my_input"] = my_input
+#     st.write("You have entered: ", my_input)

82d4fbe891eb0d4b7f44b158c3e8d526c4386e9f
Bennet Steem <bennet.steem@gmail.com>
2025-02-25 20:35:16 -0700

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -4,11 +4,11 @@ import plotly.express as px
 import altair as alt
 import matplotlib.pyplot as plt 
 from statsmodels.tsa.seasonal import seasonal_decompose 
+#from pmdarima import auto_arima
 
 st.title("Dashboard")
 
 # Amount of money spent monthly from transactional data
-# TODO: Pickup from here - https://blog.streamlit.io/crafting-a-dashboard-app-in-python-using-streamlit/
 # try:
 if True:
     df = pd.read_csv('data/transactions.csv')
@@ -87,6 +87,17 @@ if True:
 
     result = seasonal_decompose(avg_monthly_spend_per_year["Avg_Monthly_Spend"], period=12) #, model='multiplicative')
     st.pyplot(result.plot())
+
+    # TODO: Pickup from here - https://blog.streamlit.io/crafting-a-dashboard-app-in-python-using-streamlit/
+    # Fit auto_arima to avg_monthly_spend
+    # stepwise_fit = auto_arima(avg_monthly_spend_per_year["Avg_Monthly_Spend"], start_p=1, start_q=1,
+    #                           max_p=3, max_q=3, m=12,
+    #                           start_P=0, seasonal=True,
+    #                           d=None, D=1, trace=True,
+    #                           error_action='ignore',
+    #                           suppress_warnings=True,
+    #                           stepwise=True)
+    #print(stepwise_fit.summary())
 
 # except:
 #     st.text("Please upload some data to get started!!!")
